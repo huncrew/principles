7a47cd9635527523a99b3e2cce8c8e200b34a8cc
huncrew <mrdalegrant@gmail.com>
2023-04-07 09:22:23 +0100

    controller user to lowercase

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/User');
+const UserModel = require('../models/user');
 const {BadRequestError, UnauthenticatedError} = require('../errors');
 
 const register = async (req, res, next) => {
