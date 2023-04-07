6fc613a0172e0291ef05f90a5fa228f04337ecd4
huncrew <mrdalegrant@gmail.com>
2023-04-07 09:06:32 +0100

    env removed

--- routes/principles.js
+++ routes/principles.js
@@ -3,16 +3,6 @@ const router = express.Router();
 const { getPrinciples, getSpecificAreaPrinciples, createPrinciple, updatePrinciple, deletePrinciple, hardWorkPrinciple, getUsersPrinciples } = require('../controllers/principles');
 const authMiddleWare = require('../middleware/auth');
 const authenticationMiddleware = require('../middleware/auth');
-const rateLimiter = require('express-rate-limit');
-
-const apiLimiter = rateLimiter({
-  windowMs: 15 * 60 * 1000,
-  max: 10,
-  message: {
-    msg: 'Too many requests from this IP, please try again after 15 minutes',
-  },
-});
-
 
 router.get('/', getPrinciples);
 router.get('/area', getSpecificAreaPrinciples);
